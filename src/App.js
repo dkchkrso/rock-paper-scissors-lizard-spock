308ddb14de141ea41cf74aad11cb460e0b98a8ca
Christian Kroer Sørensen <dkchkrso@lego.com>
2022-08-25 13:52:17 +0200

    scss added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from "./images/icon-spock.svg";
 
-import "./App.css";
+import lizard from "./images/icon-lizard.svg";
+import paper from "./images/icon-paper.svg";
+import rock from "./images/icon-rock.svg";
+import scissors from "./images/icon-scissors.svg";
+import spock from "./images/icon-spock.svg";
+
+import "./styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="container">
+      <img src={lizard} alt="lizard" />
+      <img src={paper} alt="paper" />
+      <img src={rock} alt="rock" />
+      <img src={scissors} alt="scissors" />
+      <img src={spock} alt="spock" />
+      <h1>, TE, ST</h1>
+      <p>P-tag TEXT</p>
     </div>
   );
 }
